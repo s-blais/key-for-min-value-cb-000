205b728047db71e30690a6be9f446791e811f5e8
Stephen Blais <stephen@s-blais.com>
2019-08-23 18:55:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   # test_key = "x"
   lowest_value = nil
-  lowest_value_key = nil
+  # lowest_value_key = nil
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
       lowest_value_key = key
